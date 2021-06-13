7aae7d4cd62b93ede521e79c1ad5708593fd8545
oksazi1231 <85841153+oksazi1231@users.noreply.github.com>
2021-06-13 23:52:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bé ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnYes: 'Hãy chọn yêu anh để đẫn tới con đường hạnh phúc',
+    btnNo: 'Không, Em thử chọn không đi rùi biết',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bé lại yêu anh <3',
+    btnReply: 'Gửi lời yêu cho anh nhé ❤️',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
+    mess: 'Anh yêu em nhất quả đất này 😘😘',
+    messDesc: 'Nguyện cùng anh đi tiếp chặng đượng còn lại nhé bé.',
+    btnAccept: 'Hãy nói đồng ý đi nè',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
